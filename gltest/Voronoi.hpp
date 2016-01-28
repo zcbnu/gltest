17e2e096b690939e2ab6c64f2309ec96a57aa9e0
zcbnu <huangbaoying@oasgames.com>
2016-01-28 19:03:02 +0800

    -update

--- gltest/Voronoi.hpp
+++ gltest/Voronoi.hpp
@@ -11,6 +11,8 @@
 
 #include <stdio.h>
 #include <vector>
+#include <list>
+#include <iterator>
 
 template<typename T>
 class Point
@@ -34,17 +36,76 @@ private:
     int _nV;
 };
 
-class BTree;
 struct BTreeNode{
-    BTreeNode* _l;
-    BTreeNode* _r;
+    static const int CHILD_NUM = 2;
+    BTreeNode* _c[CHILD_NUM];
     BTreeNode* _p;
     BTreeNode* _n;
-    friend class BTree;
-} BTreeNode;
+};
+
+template<typename T>
+class Leaf : public BTreeNode
+{
+public:
+    Point<T> point;
+};
+
+template <class T>
+class Branch : public BTreeNode
+{
+    Point<T> _1;
+    Point<T> _2;
+public:
+    Branch(Point<T> p1, Point<T> p2) :_1(p1), _2(p2) {}
+};
+
 
 class BTree{
+public:
+    typedef BTreeNode * iterator;
+    iterator top() {return _root;}
+    iterator _to_iter(BTreeNode * node) {return node;}
+    iterator _set(iterator &_iter, BTreeNode * node, uint32_t idx)
+    {
+        
+        if (_iter)
+        {
+            iterator old = _iter->_c[idx];
+            _iter->_c[idx] = node;
+            if (node)
+            {
+                node->_p = _iter->_c[idx];
+            }
+            
+            if (old) delete old;
+        }
+        
+        return _to_iter(node);
+    }
+    iterator setL(iterator &_iter, BTreeNode * node)
+    {
+        return _set(_iter, node, 0);
+    }
+    iterator setR(iterator &_iter, BTreeNode * node)
+    {
+        return _set(_iter, node, 1);
+    }
     
+private:
+    BTreeNode* _root;
 };
+
+template <class T>
+Branch<T> *make_branch(Point<T> p1, Point<T> p2)
+{
+    return new Branch<T>(p1, p2);
+}
+
+template <class T>
+BTreeNode * add_new_site(Leaf<T> &_base, Leaf<T> &site)
+{
+    auto b = make_branch(_base.point, site.point);
+    
+}
 
 #endif /* Voronoi_hpp */
